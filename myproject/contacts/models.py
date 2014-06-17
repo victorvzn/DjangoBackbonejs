1e2d64173f58988509887d7cb2071447e58d5a10
Victor Villazon <victorzn23@gmail.com>
2014-06-17 11:39:53 -0500

    Ejercicio funcionando

--- myproject/contacts/models.py
+++ myproject/contacts/models.py
@@ -8,4 +8,4 @@ class Contact(models.Model):
     phone_number = models.CharField(max_length=20)
 
     def __unicode__(self):
-        return "%s %s" % (first_name, last_name,)
+        return "%s %s" % (self.first_name, self.last_name,)
